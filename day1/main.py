79c48b4c1b3fa4867a4cb26e216a2166d5250aab
Kevin Kibler <kjkibler13@gmail.com>
2020-12-02 00:20:12 -0600

    feat: day 1 part 2 complete

--- day1/main.py
+++ day1/main.py
@@ -5,19 +5,19 @@
 from input_helper import get_input
 from sort_array import sort
 from find_pairs import find_pairs
-from max_product import max_product
+from find_triple import find_triple
+from max_product import max_product, max_triple_product
 
 def main():
     input_array = get_input("input.txt")
-
     sort(input_array)
-    print(input_array)
-
     pairs_list = find_pairs(input_array)
-    print(pairs_list)
-
     product = max_product(pairs_list)
     print(product)
+
+    triples_list = find_triple(input_array)
+    product2 = max_triple_product(triples_list)
+    print(product2)
 
 if __name__ == "__main__":
     main()
--- day1/max_product.py
+++ day1/max_product.py
@@ -11,3 +11,13 @@ def max_product(input_pairs_array):
             max_product = product
     
     return max_product
+
+def max_triple_product(input_triples_array):
+    max_product = -1
+
+    for triple in input_triples_array:
+        product = triple[0] * triple[1] * triple[2]
+        if product > max_product:
+            max_product = product
+    
+    return max_product
